6ebcb421218f841979771592a3a6b5c0ed181892
ahmedwaqas92 <waqaschief@gmail.com>
2024-07-24 18:07:54 +0800

    Initial Commit, Directory Structure

---
+++ libraries/libraries.py
@@ -0,0 +1,25 @@
+import os
+import re
+import csv
+import uuid
+import json
+import time
+import base64
+import hashlib
+import sqlite3
+import logging
+import requests
+import numpy as np
+from flask_session import Session
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from collections import defaultdict
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from flask import Flask, render_template, url_for, Blueprint, redirect, request, session, jsonify
