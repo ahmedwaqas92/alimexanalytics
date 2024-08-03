d52c10c01240292d9fb2e0b8e32a9f0bc2c8c7fe
ahmedwaqas92 <waqaschief@gmail.com>
2024-08-03 17:34:16 +0800

    Selenium No Sandbox Flag

--- processes/datafetch/datafetch.py
+++ processes/datafetch/datafetch.py
@@ -108,6 +108,8 @@ class tulipFetch():
         options = webdriver.ChromeOptions()
         options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36")
         options.add_argument("--headless")
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(options=options)
         try:
             self.driver.get(endpoint)
