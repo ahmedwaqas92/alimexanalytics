a77c6dc6ac2d218f3b272b910ed5b02a5bd538fa
ahmedwaqas92 <waqaschief@gmail.com>
2024-08-05 05:55:13 +0800

    increasing time

--- processes/datafetch/datafetch.py
+++ processes/datafetch/datafetch.py
@@ -93,9 +93,9 @@ class tulipFetch():
                 pagination_controls = self.driver.find_element(By.CSS_SELECTOR, '[data-testid="pagination-controls"]')
                 next_button = pagination_controls.find_elements(By.TAG_NAME, "button")[-1]
                 if "See more pages" in next_button.get_attribute("aria-label"):
-                    time.sleep(1)
+                    time.sleep(3)
                     next_button.click()
-                    time.sleep(1)
+                    time.sleep(3)
                 else:
                     final_page = int(next_button.get_attribute("aria-label").split("e ")[1])
                     last_page = int(final_page)
@@ -109,10 +109,6 @@ class tulipFetch():
         options.add_argument('--no-sandbox')
         options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36")
         options.add_argument("--headless")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--remote-debugging-port=9222")
-        # options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(options=options)
         try:
             self.driver.get(endpoint)
@@ -120,16 +116,16 @@ class tulipFetch():
                 self.driver.find_element(By.CSS_SELECTOR, '[data-testid="login-username"]').send_keys(email)
                 self.driver.find_element(By.CSS_SELECTOR, '[data-testid="login-password"]').send_keys(password)
                 self.driver.find_element(By.CSS_SELECTOR, '[data-testid="login-submit"]').click()
-                time.sleep(1)
+                time.sleep(3)
                 try:
                     self.driver.execute_script(f"window.location.href='{payload['workorderhistory']}';")
-                    time.sleep(1)
+                    time.sleep(3)
                     self.driver.find_element(By.XPATH, '//*[@id="tables-overview-table-Q64ewaqgPB2yfNjGS"]').click()
-                    time.sleep(1)
+                    time.sleep(3)
                     try:
-                        time.sleep(1)
+                        time.sleep(3)
                         self.driver.find_element(By.XPATH, '//*[@id="tulip-root"]/div[2]/div/div[2]/div[2]/div/div/div/div[1]/div[1]/div[1]/div[2]/div[2]/button').click()
-                        time.sleep(1)
+                        time.sleep(3)
                         self.driver.find_element(By.XPATH, '/html/body/div[13]/div/ul/li[5]/button').click()
                         time.sleep(3)
                         self.pagination()
